9e119b377d7e84726471fb74a173f556a5adbfde
KrystianGrabowski <krystian987543@gmail.com>
2020-02-28 19:01:34 +0100

    Zadanie testowe sprawdzaczka

--- pracownia/zadanie0/program.cpp
+++ pracownia/zadanie0/program.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("%d", 22);
+    int a, b;
+    if (!scanf("%d %d", &a, &b)) { return 0; }
+    if (a > b) {
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+    for (int i=a; i<=b; i++) {
+        printf("%d\n", i);
+    }
+    
     return 0;
 }
